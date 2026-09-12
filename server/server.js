b8781dc57710155ad76dae8822e0bb5d2629c3de
Alen Joshi <alenjoshy04@gmail.com>
2026-09-12 07:55:30 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -33,6 +33,13 @@ app.get('/api/health', (req, res) => {
   });
 });
 
+// Serve frontend in production
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
